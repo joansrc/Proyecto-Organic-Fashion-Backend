de51e386f2c2b608ef3b67c4dd554c11384a70a1
Joan Rosales <joanrosales.10@gmail.com>
2021-10-11 18:56:30 -0500

    create get and test in postman

--- controllers/modvendors-controller.js
+++ controllers/modvendors-controller.js
@@ -3,13 +3,26 @@ const vendors = [];
 //Post
 const createVendor = (request, response) =>{
     const vendor = request.body;
-    console.log(vendor);
-    vendors.push(vendor);
-    return response.send({ok: true, vendor, vendors});
+    //console.log(vendor);
+
+    if (!vendor.cedula){
+        return response.status(400).send({ ok: false });
+
+    }
+
+    const newVendor = {
+        ...vendor,
+        id: new Date().getTime(),
+    };
+
+    vendors.push(newVendor);
+    return response.send({ok: true, vendor: newVendor, vendors});
 };
 
 //Get
-const readVendor = () => {};
+const readVendors = (request, response) => {
+    return response.send({ok:true, vendors});
+};
 
 //Patch
 const updateVendor = () => {};
@@ -19,7 +32,7 @@ const deleteVendor = () => {};
 
 module.exports = {
     createVendor,
-    readVendor,
+    readVendors,
     updateVendor,
     deleteVendor,
 }
--- routes/modvendors-routes.js
+++ routes/modvendors-routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { createVendor } = require('../controllers/modvendors-controller');
+const { createVendor, readVendors } = require('../controllers/modvendors-controller');
 
 //Router
 const router = express.Router();
 router.post('/', createVendor);
+router.get('/', readVendors);
 
 module.exports = router;
